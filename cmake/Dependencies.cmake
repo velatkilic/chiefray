3fa3dac0cf986c77c9316009687de1929c12e818
Velat Kilic <velatkilic@gmail.com>
2026-03-31 23:46:35 -0700

    stop using cmake as a package manager

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,16 +1,4 @@
-include(FetchContent)
-include(CheckLanguage)
-
-# Try to find packages first, if not fetch
-find_package(Eigen3 CONFIG QUIET)
-if(NOT TARGET Eigen3::Eigen)
-    FetchContent_Declare(
-        eigen
-        GIT_REPOSITORY https://gitlab.com/libeigen/eigen.git
-        GIT_TAG 5.0.1
-    )
-    FetchContent_MakeAvailable(eigen)
-endif()
+find_package(Eigen3 CONFIG REQUIRED)
 
 if(CHIEFRAY_ENABLE_OMP)
     find_package(OpenMP REQUIRED)
@@ -21,32 +9,17 @@ if(CHIEFRAY_ENABLE_MPI)
 endif()
 
 if(CHIEFRAY_ENABLE_CUDA)
+    include(CheckLanguage)
     check_language(CUDA)
     enable_language(CUDA)
     find_package(CUDAToolkit REQUIRED)
 endif()
 
 if(CHIEFRAY_ENABLE_TESTS)
-    find_package(Catch2 3 CONFIG QUIET)
-    if(NOT TARGET Catch2::Catch2WithMain)
-        FetchContent_Declare(
-        Catch2
-        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG v3.13.0
-        )
-        FetchContent_MakeAvailable(Catch2)
-    endif()
+    find_package(Catch2 3 CONFIG REQUIRED)
 endif()
 
 if(CHIEFRAY_ENABLE_PYTHON)
-    find_package(pybind11 CONFIG QUIET)
-    if(NOT TARGET pybind11::module)
-        FetchContent_Declare(
-        pybind11
-        GIT_REPOSITORY https://github.com/pybind/pybind11.git
-        GIT_TAG v3.0.1
-        )
-        FetchContent_MakeAvailable(pybind11)
-    endif()
-    set(CMAKE_POSITION_INDEPENDENT_CODE ON) # needed for python bindings
+    find_package(pybind11 CONFIG REQUIRED)
+    set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 endif()
--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,20 +1,22 @@
 function(target_enable_sanitizers tgt)
-  # Check if our custom option is enabled and we are in a Debug build
-  if(CHIEFRAY_ENABLE_SANITIZERS AND CMAKE_BUILD_TYPE STREQUAL "Debug")
-    
-    if(MSVC)
-      target_compile_options(${tgt} PRIVATE /fsanitize=address /Zi)
-      # MSVC automatically links the ASan runtime when the compiler flag is set
-      set_target_properties(${tgt} PROPERTIES MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
-      target_link_options(${tgt} PRIVATE /INCREMENTAL:NO)
-      
-    else()
-        # GCC/Clang-specific configuration
-        target_compile_options(${tgt} PRIVATE
-            $<$<COMPILE_LANGUAGE:C,CXX>:-fsanitize=address,undefined>
-        )
-        target_link_options(${tgt} PRIVATE -fsanitize=address,undefined)
-    endif()
-    
+  if(NOT CHIEFRAY_ENABLE_SANITIZERS)
+    return()
+  endif()
+
+  if(MSVC)
+    target_compile_options(${tgt} PRIVATE
+      $<$<CONFIG:Debug>:/fsanitize=address /Zi>
+    )
+    set_target_properties(${tgt} PROPERTIES MSVC_DEBUG_INFORMATION_FORMAT ProgramDatabase)
+    target_link_options(${tgt} PRIVATE
+      $<$<CONFIG:Debug>:/INCREMENTAL:NO>
+    )
+  else()
+    target_compile_options(${tgt} PRIVATE
+      $<$<AND:$<CONFIG:Debug>,$<COMPILE_LANGUAGE:C,CXX>>:-fsanitize=address,undefined>
+    )
+    target_link_options(${tgt} PRIVATE
+      $<$<CONFIG:Debug>:-fsanitize=address,undefined>
+    )
   endif()
 endfunction()
